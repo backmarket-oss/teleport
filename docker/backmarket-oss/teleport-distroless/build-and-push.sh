f52886dcd3f8ac8db42a3c44afe6aac1b99e4181
maximumG <maxime.gerges@backmarket.com>
2025-07-04 15:29:10 +0200

    feat(db): support AlloyDB integration in v17.5.4
    Teleport db service implementation for GCP CloudSQL is not that far from AlloyDB. The GCP auth token requested by the DB proxy to GCP IAM just needs an additionnal `scope` to make it work with AlloyDB.

---
+++ docker/backmarket-oss/teleport-distroless/build-and-push.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+TMP_DIR="$SCRIPT_DIR/.tmp"
+CURRENT_BRANCH=$( git rev-parse --abbrev-ref HEAD )
+
+rm -rf "$TMP_DIR"
+mkdir -p "$TMP_DIR"
+cd "$TMP_DIR" || exit 1
+
+tar -czf teleport-${CURRENT_BRANCH}-linux-amd64-bin.tar.gz -C ../../../../build teleport tctl tbot tsh fdpass-teleport teleport-update
+gh release create ${CURRENT_BRANCH} --generate-notes --repo backmarket-oss/teleport ./teleport-${CURRENT_BRANCH}-linux-amd64-bin.tar.gz
+
+tar xzvf teleport-${CURRENT_BRANCH}-linux-amd64-bin.tar.gz
+
+cd $SCRIPT_DIR
+docker buildx build "$SCRIPT_DIR" -f Dockerfile --platform linux/amd64 -t "europe-docker.pkg.dev/teleport-prod-sijf/docker/teleport-distroless:${CURRENT_BRANCH}" --push
+docker buildx build "$SCRIPT_DIR" -f Dockerfile-debug --platform linux/amd64 -t "europe-docker.pkg.dev/teleport-prod-sijf/docker/teleport-distroless-debug:${CURRENT_BRANCH}" --push
+
+rm -rf "$TMP_DIR"
